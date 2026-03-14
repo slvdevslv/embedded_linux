c612f9b77f5b6311a744f30d39011106ea42017b
slvdevslv <slvdev.slv@gmail.com>
2026-03-14 20:28:29 +0530

    added write1.c

---
+++ write1.c
@@ -0,0 +1,14 @@
+#include <unistd.h> // Header for POSIX system calls
+#include <string.h> // Header for strlen
+
+int main() {
+    char *message = "Hello from the Linux Kernel!\n";
+    /* The write() system call parameters:
+       1. File Descriptor (1 is stdout / the terminal)
+       2. Pointer to the message buffer
+       3. Number of bytes to write
+    */
+    write(1, message, strlen(message),strlen(msg));
+
+    return 0;
+}
